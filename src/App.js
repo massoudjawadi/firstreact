2a2fea37150a9d3e8678586a1a0225f40d57f667
unknown <soflarteur@gmail.com>
2023-05-07 15:36:19 +0200

    add name and age

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name = "kais"
+  var age = "30"
   return (
     <div className="App">
+      <h1>my name is {name} and my age is {age}</h1>
       
     </div>
   );
